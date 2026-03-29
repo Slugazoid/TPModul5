752076c3cf991ce57907156f8398cabed3441136
Albert Sebastian <albertsebastian2301@gmail.com>
2026-03-29 17:04:50 +0700

    menambahkan class DataGeneric

--- TP_MODUL5_103022400122/Program.cs
+++ TP_MODUL5_103022400122/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace TP_MODUL5_103022400122
 {
+    class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + this.data);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            String nim = "103022400122";
+            DataGeneric<String> data = new DataGeneric<string>(nim);
+            data.PrintData();
+            Console.ReadLine();
         }
     }
 }
